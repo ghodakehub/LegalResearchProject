86a4fc7858d9eef56cbcfbd8caeb9249c548dcc6
ghodakehub <ghodake6896@gmail.com>
2025-06-25 21:20:26 +0530

    Update BaseLib.java
    add import webdriver

--- src/main/java/generic/BaseLib.java
+++ src/main/java/generic/BaseLib.java
@@ -2,7 +2,7 @@ package generic;
 
 import java.time.Duration;
 import java.util.concurrent.TimeUnit;
-
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
